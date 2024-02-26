2f8114b735f940907310bc42ffd8ef3d86727618
Annie Bonav <anniebonav@outlook.com>
2024-02-26 11:42:31 -0600

    Adds working alg

--- WorkingAlg/RBFS.py
+++ WorkingAlg/RBFS.py
@@ -2,46 +2,43 @@ from Node import Node
 from State import State
 from Problem import GraphProblem
 import json
-# problem: a problem that can be solved with a recursive best first search, in this case, a graph that represents cities locations and distances between them
+verbose = False
 
-verbose = True
-with open('./RBFS/data.json') as file:
+print("\nWELCOME TO RECURSIVE BEST FIRST SEARCH! (only heuristics)")
+
+with open('./WorkingAlg/data.json') as file:
     data = json.load(file)
 
 testProblemA = GraphProblem(State("A"), State("E"), data["testDataOneE"])
 testProblemB = GraphProblem(State("A"), State("E"), data["testDataTwoE"])
 
-testProblemC = GraphProblem(State("A"), State("H"), data["testDataLastNode"])
+testDataLastNode = GraphProblem(State("A"), State("G"), data["testDataLastNode"])
 presentationExample = GraphProblem(State("A"), State("M"), data["presentationExample"])
 
 allExpandedNodes = []
-allVisitedNodes = []
 
 def RecursiveBestFirstSearch(problem: GraphProblem) -> str:
-    global allExpandedNodes, allVisitedNodes
+    global allExpandedNodes
     allExpandedNodes = []
-    allVisitedNodes = []
 
     rbfsResult = RBFS(problem, Node(problem.initialState, 0), float('inf'))
     if rbfsResult[0] == None:
         return "No solution found"
     
     solution = rbfsResult[0].getSolution()
-    cost = rbfsResult[1]
 
-    return f"Here is the result: {solution}"
+    return f"Here is the result: {problem.initialState.name} -> {solution}"
 
 def RBFS(problem : GraphProblem, node : Node, f_limit) -> Node:
-    global allExpandedNodes, allVisitedNodes
+    global allExpandedNodes
     if problem.goalTest(node.state):
         return node, node.fCost 
     
-    allVisitedNodes.append(node.state.name)
     successors = node.expand(problem, verbose)
     for succesor in successors:
         if verbose : print("Succesor:",succesor)
-        # succesor.hCost = (max(succesor.fCost, node.fCost))
-        allExpandedNodes.append(succesor.state.name)
+        succesor.hCost = (max(succesor.fCost, node.fCost))
+        allExpandedNodes.append(succesor.state.name) ## Visualization
 
     if len(successors) == 0:
         return None, float('inf')
@@ -55,26 +52,20 @@ def RBFS(problem : GraphProblem, node : Node, f_limit) -> Node:
         alternative = successors[1].fCost if len(successors) > 1 else float('inf')
         result, best.fCost = RBFS(problem, best, min(f_limit, alternative))
 
-        if result is not None: # if result would also work
+        if result is not None:
             return result, best.fCost
     
 resultA = RecursiveBestFirstSearch(testProblemA)
 resultB = RecursiveBestFirstSearch(testProblemB)
-resultC = RecursiveBestFirstSearch(testProblemC)
+resultC = RecursiveBestFirstSearch(testDataLastNode)
 presentationResults = RecursiveBestFirstSearch(presentationExample)
 
-resultsToShow = [[resultA, f"Results A from {testProblemA.initialState.name} to {testProblemA.goalState.name}"], [resultB, f"Results B from {testProblemB.initialState.name}"], [resultC, f"Results C from {testProblemC.initialState.name} to {testProblemC.goalState.name}"], [presentationResults, f"Presentation Example Results from {presentationExample.initialState.name} to {presentationExample.goalState.name}"]]
+resultsToShow = [[resultA, f"Results A from {testProblemA.initialState.name} to {testProblemA.goalState.name}"], [resultB, f"Results B from {testProblemB.initialState.name} to {testProblemB.goalState.name}"], [resultC, f"Results C from {testDataLastNode.initialState.name} to {testDataLastNode.goalState.name}"], [presentationResults, f"Presentation Example Results from {presentationExample.initialState.name} to {presentationExample.goalState.name}"]]
 
-def nodesString(stringToShow) -> str:
+def nodesString() -> str:
     nodesString = ""
-    match (stringToShow):
-        case "visited":
-            nodesToTurnToString = allVisitedNodes
-        case "expanded":
-            nodesToTurnToString = allExpandedNodes
-        case _:
-            return "Invalid stringToShow value"
-    
+    nodesToTurnToString = allExpandedNodes
+
     for i in range(len(nodesToTurnToString)):
         if i == len(nodesToTurnToString)-1:
             return nodesString + nodesToTurnToString[i]
@@ -83,4 +74,4 @@ def nodesString(stringToShow) -> str:
     return nodesString
 
 for result in resultsToShow:
-    print(f"\n{result[1]}", result[0], f"All #{len(allVisitedNodes)} visited nodes: {nodesString('visited')}", f"All #{len(allExpandedNodes)} expanded nodes: {nodesString('expanded')}", sep="\n")
+    print(f"\n{result[1]}", result[0], f"All #{len(allExpandedNodes)} expanded nodes: {nodesString()}", sep="\n")
